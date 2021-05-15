fc800f905616d937d7c076aeeb03e4460575c640
samyar <75088294+samyarkd@users.noreply.github.com>
2021-05-15 15:11:32 +0430

    Update ItoT.py

--- ItoT.py
+++ ItoT.py
@@ -1,7 +1,14 @@
 from pywhatkit import image_to_ascii_art
-    
+import os
 
-image= input('image path: ')
+x = 0
+while x == 0:
+
+    image= input('image path: ')
+    if os.path.exists(image):
+        x = 1
+    else: print('No such file or dir !! Try agin')
+    
 text = 'art.txt'
 
 image_to_ascii_art(image, text)
